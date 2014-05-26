c960e86996c1822277d8ddef0a9e2a1318e6726a
Benjie Gillam <benjie@jemjie.com>
2014-05-26 19:52:37 +0100

    Overhaul; 255 PWM levels.

--- dalek/dalek.ino
+++ dalek/dalek.ino
@@ -1,57 +1,68 @@
+enum Mode {
+  NONE,
+  MOTOR,
+  LED
+};
+
 int leftForwardPin = 5;
 int leftBackwardPin = 6;
 int rightForwardPin = 9;
 int rightBackwardPin = 10;
+int ledPin = 13;
+
 int left = 0;
 int right = 0;
 unsigned int stage = 0;
 unsigned char a = 'a';
 unsigned char p = 'p';
 int val = 0;
+Mode mode = NONE;
 
 // the setup routine runs once when you press reset:
-void setup()  { 
+void setup()  {
   // declare pin 9 to be an output:
   pinMode(leftForwardPin, OUTPUT);
   pinMode(leftBackwardPin, OUTPUT);
   pinMode(rightForwardPin, OUTPUT);
   pinMode(rightBackwardPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   analogWrite(leftForwardPin, 255);
   analogWrite(leftBackwardPin, 255);
   analogWrite(rightForwardPin, 255);
   analogWrite(rightBackwardPin, 255);
   Serial.begin(9600);
-} 
+}
 
-// the loop routine runs over and over again forever:
-void loop()  {
-  
-  if (!Serial.available() > 0) {
-    return;
+void reset() {
+  stage = 0;
+  mode = NONE;
+}
+
+void cmdLED() {
+  if (val == '0') {
+    digitalWrite(ledPin, LOW);
+  } else if (val == '1') {
+    digitalWrite(ledPin, HIGH);
   }
-  val = Serial.read();
+  reset();
+}
+
+void cmdMotor() {
   val = val - a;
   if ((val < 0) || (val > p - a)) {
-    // Reset / invalid
-    stage = 0;
+    reset();
     return;
   }
-  if (stage == 0) {
-    left = val * 16;
-    if (left > 0) {
-      left += 15; // 15*16 + 15 = 255
-    }
-    stage++;
-    return;
-  } else if (stage == 1) {
-    right = val * 16;
-    if (right > 0) {
-      right += 15; // 15*16 + 15 = 255
-    }
-    stage++;
-    return;
+  stage++;
+  if (stage == 1) {
+    left = (val << 4);
+  } else if (state == 2) {
+    left += val;
+  } else if (stage == 3) {
+    right = (val << 4);
+  } else if (stage == 4) {
+    right += val;
   } else {
-    stage = 0; // Reset ready for next time
     if ((val == 1) || (val == 3)) {
       left = -left;
     }
@@ -59,39 +70,48 @@ void loop()  {
       right = -right;
     }
     if (val > 3) {
+      reset();
       return; // Invalid
     }
-  }
-  
-  if (left >= 0) {
-    analogWrite(leftForwardPin, 255 - left);
-    analogWrite(leftBackwardPin, 255);
-  } else {
-    analogWrite(leftForwardPin, 255);
-    analogWrite(leftBackwardPin, left);
-  }
-  if (right >= 0) {
-    analogWrite(rightForwardPin, 255 - right);
-    analogWrite(rightBackwardPin, 255);
-  } else {
-    analogWrite(rightForwardPin, 255);
-    analogWrite(rightBackwardPin, right);
-  }
-  
-  delay(1);
-  
-  
-  /*
-  if (left >= 255) {
-    if (right >= 255) {
-      left = -255;
-      right = -255;
+
+    if (left >= 0) {
+      analogWrite(leftForwardPin, 255 - left);
+      analogWrite(leftBackwardPin, 255);
     } else {
-      right++;
+      analogWrite(leftForwardPin, 255);
+      analogWrite(leftBackwardPin, left);
     }
-  } else {
-    left++;
+
+    if (right >= 0) {
+      analogWrite(rightForwardPin, 255 - right);
+      analogWrite(rightBackwardPin, 255);
+    } else {
+      analogWrite(rightForwardPin, 255);
+      analogWrite(rightBackwardPin, right);
+    }
+    reset();
   }
-  */
 }
 
+void loop()  {
+  if (Serial.available() <= 0) {
+    delay(1);
+    return;
+  }
+  val = Serial.read();
+  if (mode == MOTOR) {
+    cmdMotor();
+    return;
+  } else if (mode == LED) {
+    cmdLED();
+    return;
+  } else if (mode == NONE) {
+    if (val == 'M') {
+      mode = MOTOR;
+    } else if (val == 'L') {
+      mode = LED;
+    }
+    return;
+  }
+  reset();
+}
