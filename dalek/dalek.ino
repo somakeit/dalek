feb15392fbf1f776ea011b3398ae6d056c0d7dd4
Benjie Gillam <benjie@jemjie.com>
2014-06-03 21:48:16 +0100

    Add support for Benjie's L298N based bot.

--- dalek/dalek.ino
+++ dalek/dalek.ino
@@ -4,10 +4,22 @@ enum Mode {
   LED
 };
 
+// Uncomment next line if you're Benjie
+//#define L298N_MODE 1
+
+#ifdef L298N_MODE
+int leftForwardPin = 12;
+int leftBackwardPin = 11;
+int leftEnablePin = 10;
+int rightForwardPin = 8;
+int rightBackwardPin = 7;
+int rightEnablePin = 6;
+#else
 int leftForwardPin = 5;
 int leftBackwardPin = 6;
 int rightForwardPin = 9;
 int rightBackwardPin = 10;
+#endif
 int ledPin = 13;
 
 int left = 0;
@@ -21,15 +33,26 @@ Mode mode = NONE;
 // the setup routine runs once when you press reset:
 void setup()  {
   // declare pin 9 to be an output:
+  pinMode(ledPin, OUTPUT);
   pinMode(leftForwardPin, OUTPUT);
   pinMode(leftBackwardPin, OUTPUT);
   pinMode(rightForwardPin, OUTPUT);
   pinMode(rightBackwardPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+#ifdef L298N_MODE
+  pinMode(leftEnablePin, OUTPUT);
+  pinMode(rightEnablePin, OUTPUT);
+  analogWrite(leftForwardPin, 0);
+  analogWrite(leftBackwardPin, 0);
+  analogWrite(leftEnablePin, 0);
+  analogWrite(rightForwardPin, 0);
+  analogWrite(rightBackwardPin, 0);
+  analogWrite(leftEnablePin, 0);
+#else
   analogWrite(leftForwardPin, 255);
   analogWrite(leftBackwardPin, 255);
   analogWrite(rightForwardPin, 255);
   analogWrite(rightBackwardPin, 255);
+#endif
   Serial.begin(9600);
 }
 
@@ -73,7 +96,25 @@ void cmdMotor() {
       reset();
       return; // Invalid
     }
+#if L298N_MODE
+    if (left >= 0) {
+      digitalWrite(leftForwardPin, HIGH);
+      digitalWrite(leftBackwardPin, LOW);
+    } else {
+      digitalWrite(leftForwardPin, LOW);
+      digitalWrite(leftBackwardPin, HIGH);
+    }
 
+    if (right >= 0) {
+      digitalWrite(rightForwardPin, HIGH);
+      digitalWrite(rightBackwardPin, LOW);
+    } else {
+      digitalWrite(rightForwardPin, LOW);
+      digitalWrite(rightBackwardPin, HIGH);
+    }
+    analogWrite(leftEnablePin, abs(left));
+    analogWrite(rightEnablePin, abs(right));
+#else
     if (left >= 0) {
       analogWrite(leftForwardPin, 255 - left);
       analogWrite(leftBackwardPin, 255);
@@ -89,6 +130,7 @@ void cmdMotor() {
       analogWrite(rightForwardPin, 255);
       analogWrite(rightBackwardPin, right);
     }
+#endif
     reset();
   }
 }
