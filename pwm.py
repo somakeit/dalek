095fa5945d4c85a45ec645da55dc55046e792e9b
Benjie Gillam <benjie@jemjie.com>
2015-06-09 19:14:41 +0100

    Ready to rock and roll?

---
+++ pwm.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+PWM_PIN = 18
+ALL_PINS = [PWM_PIN]
+pwm = None
+
+def init():
+    GPIO.setmode(GPIO.BCM)
+
+    for port in ALL_PINS:
+        GPIO.setup(port, GPIO.OUT)
+
+    pwm = GPIO.PWM(PWM_PIN, 50)
+    pwm.start(0)
+
+def write(value):
+    pwm.ChangeDutyCycle(value * 100)
--- soundlevel.py
+++ soundlevel.py
@@ -1,41 +1,17 @@
 #!/usr/bin/python
 
-"""
-Send 8-bit PulseAudio peak sound samples to a digital-to-analog convertor
-(which is intended to be connected to an analog VU meter).
-
-Because it requires access to the GPIO pins, this program is intended to be run
-as root. After the pins are initialised, it will change uid to AUDIO_USER in
-order to sample peak audio levels from PulseAudio. Set AUDIO_USER as
-appropriate.
-"""
-
-import os
-import pwd
-
-import dac
+import pwm
 from peak_monitor import PeakMonitor
 
-
-AUDIO_USER = 'mpd'  # The user running the PulseAudio daemon that's writing to
-                    # the audio outputs
 SINK_NAME = 'alsa_output.platform-bcm2835_AUD0.0.analog-stereo'
 METER_RATE = 128    # Hz
 
-def change_uid(username):
-    account = pwd.getpwnam(username)
-    os.setuid(account.pw_uid)
-    os.environ['HOME'] = account.pw_dir
-
 def main():
-    dac.init()   # Set up the GPIO pins used to talk to the DAC
-
-    change_uid(AUDIO_USER)
+    pwm.init()
 
     for sample in PeakMonitor(SINK_NAME, METER_RATE):
-        # samples range from 0 to 127 so double them to get the full voltage
-        # range of the DAC
-        dac.write_byte(sample << 1)
+        # samples range from 0 to 127
+        pwm.write(sample/127)
 
 if __name__ == '__main__':
     main()
